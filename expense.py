39b00773588e1c8df02fdbd184e0eb11fe4cbeb5
Randjole <randjole15@gmail.com>
2025-03-14 12:57:55 +0100

    secount commit

--- expense.py
+++ expense.py
@@ -1,30 +1,128 @@
-import json 
-
-collection_exp = {'ID': 1,
-                  'Date': '13.03.2025',
-                  'Description':'Lunch',
-                  'Amount': 20}
-
-# def add_exp():
-#     expense = input('Add your type of expense: ')
-#     bill = input('Enter your bill: ')
+import json
 
 
-#     collection_exp = json.dumps({})
+
+list_exp = []
+
+
+class Expense(): 
+
+
+    def activate_exp(self): 
+    
+
+        while True:
+
+            question = input('Do you want to add your expense? [y/n]: ')
+
+            if question == 'y':
+
+                while True:
+
+                    iD = input('Your ID: ')
+                    date = input('Enter date: ')
+                    description = input('Enter description: ')
+                    amount = input ('Enter amount: ')
+
+                    list_exp.append({'ID': iD ,'Date': date, 'Description': description })
+                    breaking = input('Do you want to add more?: [y/n]')
+
+                    if breaking == 'y':
+                        continue
+
+                    elif breaking == 'n':
+                        break
+
+
+
+            elif question =='n':
+                if len(list_exp) == 0:
+                    print('Sorry your list is empty')
+
+                else:
+                    print('Thank you this is your list')
+                    print(list_exp)
+                    break
+            else:
+                print('Try again')
+
+
+    def view_exp(self):
+
+        if len(list_exp) == 0:
+            print('Sorry your list is empty')
+
+
+        else:
+            print('ID Date Desription amount')
+            for list_e in list_exp:
+                for key, value in list_e.items():
+                    print(f'{key} {value}')
+                    
+
+
+
+    
+
+
+    def main(self):
+        exp = Expense()
+
+
+        while True:
+    
+            print('**** Expense Task ****')
+            print('1. Add expense')
+            print('2. View expenses')
+
+            enter = input('Enter your choise: ')
+
+            if enter == '1':
+                exp.activate_exp()
+            if enter == '2':
+                exp.view_exp()
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    activate = Expense()
+    work = activate.main()
     
 
 
 
 
-def view_exp():
-    print('*** View of all your expenses *** ') 
-    print('ID  Date  Description  Amount')   
-    for k,v in collection_exp.items():
-        print(v, end = "   ")
 
 
 
 
 
 
-view_exp()
+
+
+        
+            # try:
+            #     with open (save +''+ '.json','w') as f:
+            #         json.dump(list_exp, f)
+
+            # except FileExistsError:
+            #     print('Sorry')  
+
+   
